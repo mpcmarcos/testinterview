c07db6928c5e3047e9d959a71cc23ec84af07038
Jader Dalmas <jader@grayboxsolutions.com>
2020-12-19 13:13:37 -0300

    add migration on db

---
+++ Data/Migrations/20201219161300_TwoFields.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Interview.Data.Migrations
+{
+    public partial class TwoFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TwoFields",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Field1 = table.Column<string>(nullable: true),
+                    Field2 = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TwoFields", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TwoFields");
+        }
+    }
+}
