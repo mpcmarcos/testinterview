cbbfb84f91bc69907627402b46bde9d94afc7a09
Jader Dalmas <jader@grayboxsolutions.com>
2020-12-19 13:18:40 -0300

    add twofields and list them

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -6,25 +6,41 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Interview.Models;
+using Interview.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Interview.Controllers
 {
-    public class TestController : Controller
+  [Authorize]
+  public class TestController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public TestController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public TestController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-
-        public IActionResult Index()
-        {
-            //You have return a view with 2 textox and a button
-            return View();
-        }
-        
-
+      _logger = logger;
+      _context = context;
     }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(TwoFieldsViewModel model)
+    {
+      _context.TwoFields.Add(new TwoFields(model));
+      _context.SaveChanges();
+
+      return View();
+    }
+
+    public IActionResult List()
+    {
+      return View(_context.TwoFields);
+    }
+  }
 }
