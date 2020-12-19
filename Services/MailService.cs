ba7d05b26b97ba7a5f0e55b0c898ef1ab0b65fd2
Jader Dalmas <jader@grayboxsolutions.com>
2020-12-19 13:18:22 -0300

    log services (avoid error only)

--- Services/MailService.cs
+++ Services/MailService.cs
@@ -1,11 +1,19 @@
+using Microsoft.Extensions.Logging;
+
 namespace Interview.Services
 {
-    public class MailService : IMailService
-    {
-        public void Send(string to, string from, string body)
-        {
-            throw new System.NotImplementedException();
-        }
+  public class MailService : IMailService
+  {
+    private readonly ILogger<MailService> _logger;
 
+    public MailService(ILogger<MailService> logger)
+    {
+      _logger = logger;
     }
+
+    public void Send(string to, string from, string body)
+    {
+      _logger.LogInformation("To: {to} | From: {from} | Body: {body}", to, from, body);
+    }
+  }
 }
--- Services/SmsService.cs
+++ Services/SmsService.cs
@@ -1,12 +1,19 @@
+using Microsoft.Extensions.Logging;
+
 namespace Interview.Services
 {
-    public class SmsService : ISmsService
-    {
-       
+  public class SmsService : ISmsService
+  {
+    private readonly ILogger<SmsService> _logger;
 
-        public void Send(string phone, string message)
-        {
-            throw new System.NotImplementedException();
-        }
+    public SmsService(ILogger<SmsService> logger)
+    {
+      _logger = logger;
     }
+
+    public void Send(string phone, string message)
+    {
+      _logger.LogInformation("Phone: {phone} | Message: {message}", phone, message);
+    }
+  }
 }
